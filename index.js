1143d8bd73a3d39805952b1e85f66ca3804579c5
Aminul Islam <aminulislamemon2002@gmail.com>
2025-09-20 19:30:07 +0600

    setup projects

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config({ quiet: true });
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Importing routes
+const totoRoutes = require("./routers/todo.router");
+
+// Middleware
+app.use(express.json());
+
+// Mongoose connection
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fsd9z3z.mongodb.net/testTodos`
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+// Routes setup
+app.use("/api/todo", totoRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ routers/todo.router.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// Define routes
+
+// Insert a new todo
+router.post("/", (req, res) => {});
+
+module.exports = router;
